2088e5387f01f81256469dfed08e46480335ffad
hackmini <f416720001@gmail.com>
2023-09-04 00:53:25 +0800

    Refactor: project module

---
+++ src/huggingface/__init__.py
@@ -0,0 +1,3 @@
+from .utils.storages import tmp_file  # noqa: F401
+from .blip_image_captioning_large import blip_image_captioning_large  # noqa: F401
+from .yolos_tiny import yolos_tiny  # noqa: F401
